47ebf898e9a208f2ff367fca578404113ba08cee
Vcrostin <kostyash2001@mail.ru>
2022-04-13 10:36:05 +0300

    move type to card basic class

--- core/card_types/card.h
+++ core/card_types/card.h
@@ -3,17 +3,24 @@
 //
 #pragma once
 #include <string>
+#include <array>
 
 namespace Core {
-    enum class CARD_TYPE {
-        UNKNOWN = 0,
-        COMMON = 1,
-        RARE = 2,
-        EPIC = 3,
-        LEGENDARY = 4
-    };
 
     class Card {
+    public:
+        enum class CARD_TYPE {
+            UNKNOWN = 0,
+            COMMON = 1,
+            RARE = 2,
+            EPIC = 3,
+            LEGENDARY = 4
+        };
+    protected:
+        static const int CARD_WIDTH = 30;
+        static const int CARD_HEIGHT = 50;
+        Card(int32_t cost, CARD_TYPE cardType);
+
     public:
         [[nodiscard]]
         constexpr inline int32_t GetCost() const {
@@ -25,11 +32,15 @@ namespace Core {
             return cardType;
         }
 
+        [[nodiscard]]
+        inline virtual std::array<std::array<int,CARD_WIDTH>, CARD_HEIGHT> ShowCard() const {
+            return {{}};
+        }
+
+        // formatted output this is based
+        // protected with args and use??
+        // virtual still usefull
         [[nodiscard]] virtual std::string ShowInfo() const = 0;
-
-    protected:
-
-        Card(int32_t cost, CARD_TYPE cardType);
 
     private:
         int32_t cost;
