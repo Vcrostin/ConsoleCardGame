703a3ac29457f516386f164348dab40de1a1109b
Vcrostin <kostyash2001@mail.ru>
2022-06-07 12:10:24 +0300

    make config path correct

--- core/configurations/conf_parser.h
+++ core/configurations/conf_parser.h
@@ -3,6 +3,7 @@
 //
 
 #pragma once
+
 #include <filesystem>
 #include <fstream>
 #include <iomanip>
@@ -12,6 +13,7 @@
 #include <unordered_map>
 #include <utility>
 #include <variant>
+#include "../utils/path_manip.h"
 
 using std::string;
 using std::ifstream;
@@ -22,25 +24,24 @@ using std::pair;
 
 class ConfParser {
 private:
-    decltype(auto) GetValue (const string& key) {
+    decltype(auto) GetValue(const string &key) {
         return dict[key];
     }
 
-    decltype(auto) GetValue (const string& key) const {
+    decltype(auto) GetValue(const string &key) const {
         return dict.at(key);
     }
 
     void ParseFile() {
         string line;
-        while(std::getline(fin, line)) {
+        while (std::getline(fin, line)) {
             std::istringstream curLine(line);
             if (std::string type; std::getline(curLine, type, ':')) {
                 if (std::string key; std::getline(curLine, key, '=')) {
                     if (std::string value; std::getline(curLine, value)) {
                         if (type == "int32") {
                             dict[key] = std::stoi(value);
-                        }
-                        else if (type == "string") {
+                        } else if (type == "string") {
                             dict[key] = value;
                         }
                     }
@@ -51,31 +52,33 @@ private:
 
 public:
     typedef variant<int32_t, string> valueType;
-    explicit ConfParser(const string& fileName) : fin(fileName) {
+
+    explicit ConfParser(const string &fileName) : fin(fileName) {
         if (!fin.is_open()) {
             throw std::invalid_argument("file not found");
         }
         ParseFile();
     }
 
-    int32_t GetInt(const string& key) {
+    int32_t GetInt(const string &key) {
         return get<int32_t>(GetValue(key));
     }
 
-    string GetString(const string& key) {
+    string GetString(const string &key) {
         return get<string>(GetValue(key));
     }
 
-    friend std::ostream& operator << (std::ostream& stream, const ConfParser& confParser);
+    friend std::ostream &operator<<(std::ostream &stream, const ConfParser &confParser);
+
 private:
     ifstream fin;
     unordered_map<string, valueType> dict;
 };
 
-std::ostream& operator << (std::ostream& stream, const ConfParser& confParser) {
-    for (const auto& [key, value] : confParser.dict) {
+std::ostream &operator<<(std::ostream &stream, const ConfParser &confParser) {
+    for (const auto &[key, value]: confParser.dict) {
         stream << "|" << std::left << std::setw(14) << key << "|";
-        std::visit([&stream] (const auto& val) {
+        std::visit([&stream](const auto &val) {
             stream << std::left << std::setw(14) << val << "|\n";
         }, value);
     }
@@ -84,4 +87,4 @@ std::ostream& operator << (std::ostream& stream, const ConfParser& confParser) {
 }
 
 // TODO: ether ad correct path to configure directory ether copy file to specific directory each time
-std::string CONF_PATH = std::filesystem::current_path();
+std::string CONF_PATH = PathManip().ConcatenateByLastPart(PathManip{"HearthtoneConsole/*/core/configurations"});
