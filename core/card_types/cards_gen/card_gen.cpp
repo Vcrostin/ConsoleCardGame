c26522052ec72eae22aa33c42cf249fc487d70ce
Vcrostin <kostyash2001@mail.ru>
2022-06-16 14:07:37 +0300

    introduce python codegen classes

--- core/card_types/cards_gen/card_gen.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by brostin on 27.04.22.
-//
-
-#include "card_gen.h"
-
-constexpr Core::Gen::CardGen::CardGen(const std::string &cardType, std::unordered_map<std::string, std::string> &) {
-    // if constexpr (cardType == Core::Card::)
-}
---
+++ core/card_types/cards_gen/main.py
@@ -0,0 +1,77 @@
+import os
+import re
+import shutil
+
+
+def camel_to_snake(name):
+    name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
+
+
+if __name__ != '__main__':
+    raise RuntimeError("This script supposed to run on its own")
+
+LIST_OF_DATA = [["UnitCard", "Unit", 1, 1, "COMMON"], ]
+
+dir_path = os.path.join(os.path.dirname(os.getcwd()), "generated_cards")
+print(dir_path)
+if os.path.exists(dir_path):
+    shutil.rmtree(dir_path)
+os.mkdir(dir_path)
+
+format_template_header = """//
+// Created by codegen
+//
+#pragma once
+
+#include "../card_type.h"
+#include "../{base_class_snake}.h"
+
+namespace Core {{
+    class {new_class_camel} : public Core::{base_class_camel} {{
+    public:
+        {new_class_camel}();
+    }};
+}}
+
+"""
+
+
+def generate_header(camel_base, camel_new):
+    snake_base = camel_to_snake(camel_base)
+    snake_new = camel_to_snake(camel_new)
+    with open(os.path.join(dir_path, snake_new + '.h'), 'w') as file:
+        write_str = format_template_header.format(base_class_snake=snake_base, new_class_camel=camel_new,
+                                                  base_class_camel=camel_base)
+        file.write(write_str)
+
+
+format_template_source = """//
+// Created by codegen
+//
+
+#include "{new_class_snake}.h"
+
+namespace Core {{
+    {new_class_camel}::{new_class_camel}({new_class_arguments}) : {base_class_camel}({new_class_values}) {{
+
+    }}
+}}
+
+"""
+
+
+# TODO: add parser base and cur classes
+def generate_source(camel_base, camel_new, arguments_base, initialization_list_arguments):
+    snake_new = camel_to_snake(camel_new)
+    with open(os.path.join(dir_path, snake_new + '.cpp'), 'w') as file:
+        write_str = format_template_source.format(new_class_snake=snake_new, new_class_camel=camel_new,
+                                                  base_class_camel=camel_base, new_class_arguments=arguments_base,
+                                                  new_class_values=initialization_list_arguments)
+        file.write(write_str)
+
+
+# TODO: add cmake file
+for ls in LIST_OF_DATA:
+    generate_header(ls[0], ls[1])
+    generate_source(ls[0], ls[1], "", "")
