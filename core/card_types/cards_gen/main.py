70c9a4a2de9ae9fc40589e32250cbc3c89ea1314
Vcrostin <kostyash2001@mail.ru>
2022-06-19 17:13:36 +0300

    add base generated file of each

--- core/card_types/cards_gen/main.py
+++ core/card_types/cards_gen/main.py
@@ -115,6 +115,7 @@ add_library(GeneratedCards
 """
 
 
+# TODO add all_new_classes_include file
 def generate_cmake(source_files):
     with open(os.path.join(dir_path, 'CMakeLists.txt'), 'w') as file:
         write_str = format_template_cmake.format(source_files='\n'.join(list(map(lambda x: ' '.join(x), source_files))))
---
+++ core/card_types/generated_cards/recovery.cpp
@@ -0,0 +1,11 @@
+//
+// Created by codegen
+//
+
+#include "recovery.h"
+
+namespace Core {
+    Recovery::Recovery() : RecoveryCard(1, 1, Core::Card::CARD_RARITY::COMMON) {
+
+    }
+}
