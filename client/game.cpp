d7df494af908b7e8c1d8192a87caa97115bca794
Vcrostin <kostyash2001@mail.ru>
2022-06-22 17:00:34 +0300

    preparing ui pt1

---
+++ client/game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by brostin on 22.06.22.
+//
+
+#include "game.h"
--- core/user.h
+++ core/user.h
@@ -3,26 +3,32 @@
 //
 
 #pragma once
+
 #include <string>
 #include <string_view>
+#include <unordered_map>
+#include "card_types/generated_cards/all_generated_classes.h"
 
 namespace Core {
     class User {
     protected:
     public:
-        enum class UserStatus{
+        enum class UserStatus {
             USER = 0,
             MODERATOR = 1,
             ADMIN = 2
         };
     private:
-        std::string login;
-        uint64_t sessionId;
+        // std::string login;
+        // uint64_t sessionId;
         std::string userName;
-        int32_t level;
-        UserStatus userStatus;
+        std::array<std::shared_ptr<UnitCard>, 2> deskCards;
+        std::array<std::shared_ptr<Card>, 5> handCards;
+        // int32_t level;
+        // UserStatus userStatus;
     public:
-        User(uint64_t sessionId, const std::string_view& userName, int32_t lvl,
-             UserStatus userStatus = Core::User::UserStatus::USER);
+//        User(uint64_t sessionId, const std::string_view& userName, int32_t lvl,
+//             UserStatus userStatus = Core::User::UserStatus::USER);
+        explicit User(const std::string_view &userName);
     };
 }
