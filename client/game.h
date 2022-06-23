b5b9b9e2542a82f50499990877aa6e840166e6b8
Vcrostin <kostyash2001@mail.ru>
2022-06-23 18:47:12 +0300

    preparing ui pt2

--- client/game.h
+++ client/game.h
@@ -1,64 +1,58 @@
 //
-// Created by brostin on 22.06.22.
+// Created by brostin on 23.06.22.
 //
 
 #pragma once
 
-#include <iostream>
-#include <iomanip>
-#include "../core/utils/colors_define.h"
 #include <boost/asio.hpp>
-
-using std::cout, std::endl;
+#include "client_sender.h"
 
 class Game {
 public:
-    explicit Game(boost::asio::io_service &ioService) : ioServise(ioService) {
+    explicit Game(boost::asio::io_service &ioService) : ioService(ioService) {
 
     }
 
-    void Start() {
-        int i = 0;
-        do {
-            PrintMenu();
-            std::cin >> i;
-            system("clear");
-            Action(i);
-        } while (i != 0);
+    void Run() {
+        auto clientSender = ClientSender::NewClient(ioService);
+        std::string msg = "To be, or not to be, that is the question:\n"
+                          "Whether 'tis nobler in the mind to suffer\n"
+                          "The slings and arrows of outrageous fortune,\n"
+                          "Or to take arms against a sea of troubles\n"
+                          "And by opposing end them. To die—to sleep,\n"
+                          "No more; and by a sleep to say we end\n"
+                          "The heart-ache and the thousand natural shocks\n"
+                          "That flesh is heir to: 'tis a consummation\n"
+                          "Devoutly to be wish'd. To die, to sleep;\n"
+                          "To sleep, perchance to dream—ay, there's the rub:\n"
+                          "For in that sleep of death what dreams may come,\n"
+                          "When we have shuffled off this mortal coil,\n"
+                          "Must give us pause—there's the respect\n"
+                          "That makes calamity of so long life.\n"
+                          "For who would bear the whips and scorns of time,\n"
+                          "Th'oppressor's wrong, the proud man's contumely,\n"
+                          "The pangs of dispriz'd love, the law's delay,\n"
+                          "The insolence of office, and the spurns\n"
+                          "That patient merit of th'unworthy takes,\n"
+                          "When he himself might his quietus make\n"
+                          "With a bare bodkin? Who would fardels bear,\n"
+                          "To grunt and sweat under a weary life,\n"
+                          "But that the dread of something after death,\n"
+                          "The undiscovere'd country, from whose bourn\n"
+                          "No traveller returns, puzzles the will,\n"
+                          "And makes us rather bear those ills we have\n"
+                          "Than fly to others that we know not of?\n"
+                          "Thus conscience doth make cowards of us all,\n"
+                          "And thus the native hue of resolution\n"
+                          "Is sicklied o'er with the pale cast of thought,\n"
+                          "And enterprises of great pith and moment\n"
+                          "With this regard their currents turn awry\n"
+                          "And lose the name of action.";
+        clientSender->AddMessage(msg);
+        clientSender->SendAll();
     }
 
 private:
-    boost::asio::io_service &ioServise;
-
-    static void PrintMenu() {
-        cout << endl << "____MENU____" << endl;
-        cout << std::right << std::setw(3) << "0 " << std::left << "Exit" << endl;
-        cout << std::right << std::setw(3) << "1 " << std::left << "Start Game" << endl;
-        cout << std::right << std::setw(3) << "2 " << std::left << "Collections" << endl;
-        cout << std::right << std::setw(3) << "3 " << std::left << "Settings" << endl;
-        cout << "____________" << endl;
-        cout << "Enter number: ";
-    }
-
-    void Action(int i) {
-        switch (i) {
-            case 0:
-                std::cerr << RED << "breaking" << WHITE << std::endl;
-                break;
-            case 1:
-                std::cerr << RED << "new game..." << WHITE << std::endl;
-                break;
-            case 2:
-                std::cerr << RED << "show collections..." << WHITE << std::endl;
-                break;
-            case 3:
-                std::cerr << RED << "settings open" << WHITE << std::endl;
-                break;
-            default:
-                std::cerr << RED << "incorrect action" << WHITE << std::endl;
-                break;
-        }
-    }
+    boost::asio::io_service &ioService;
 };
-
 
